0f4cd4165acd87faf3714dd214f0c67d71ff3847
SiDevs <welsh51@tiscali.co.uk>
2018-09-29 12:12:25 +0100

    Second upload

---
+++ SimpleCSharpApp/SimpleCSharpApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCSharpApp
+
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("***** Not My First C# App *****");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+
+            Console.ReadLine();
+
+            return -1;
+        }
+    }
+}
